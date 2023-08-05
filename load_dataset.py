93a27bc65a79cc397d61c5f0c105b782693fa643
deqg <132956809+deqg@users.noreply.github.com>
2023-08-05 11:45:07 -0500

    Update load_dataset.py

--- load_dataset.py
+++ load_dataset.py
@@ -36,8 +36,8 @@ from tensorflow.keras.utils import to_categorical
 
 # Change the dataloader using recent version of tensorflow dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-x_train = tf.reshape(x_train, [x_train.shape[0],x_train.shape[1]**2])
-x_test = tf.reshape(x_test, [x_test.shape[0],x_test.shape[1]**2])
+x_train = np.reshape(x_train, [x_train.shape[0],x_train.shape[1]**2])
+x_test = np.reshape(x_test, [x_test.shape[0],x_test.shape[1]**2])
 # y_train = to_categorical(y_train)
 # y_test = to_categorical(y_test)
 x_valid, x_train = x_train[:10000], x_train[10000:]
