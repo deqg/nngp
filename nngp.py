e347c6b248c4d79abc4adcb3410d293694b811aa
deqg <132956809+deqg@users.noreply.github.com>
2023-08-05 14:53:06 -0500

    Update nngp.py

--- nngp.py
+++ nngp.py
@@ -211,7 +211,7 @@ class NNGPKernel(object):
 
       q_ab = cov_init
       print(q_ab.shape)
-      print(self.bias_var.shape)
+      print(self.bias_var)
       print(cov_init.shape)
       q_ab = self.weight_var * q_ab + self.bias_var * cov_init # ADD cov(X,X') 
       corr = q_ab / q_aa_init[0]
