9f4ca10e71d2e0f112e7261d5ebeae1c9ae77574
deqg <132956809+deqg@users.noreply.github.com>
2023-08-05 14:59:15 -0500

    Update nngp.py

--- nngp.py
+++ nngp.py
@@ -221,10 +221,14 @@ class NNGPKernel(object):
         with tf.name_scope("q_ab"):
           q_ab_all = []
           for b_x in range(batch_count):
+            input1_batch = input1[batch_size * b_x : batch_size * (b_x + 1), :]
+            input2_batch = input2[batch_size * b_x : batch_size * (b_x + 1), :]
             with tf.name_scope("batch_%d" % b_x):
               corr_flat_batch = corr[
                   batch_size * b_x : batch_size * (b_x + 1), :]
               corr_flat_batch = tf.reshape(corr_flat_batch, [-1])
+              cov_init_batch = tf.matmul(input1_batch, input2_batch, transpose_b=True) / input1_batch.shape[1] #.value
+
 
               for l in xrange(self.depth):
                 with tf.name_scope("layer_%d" % l):
@@ -235,7 +239,7 @@ class NNGPKernel(object):
                                               xp=q_aa,
                                               yp=corr_flat_batch)
 
-                  q_ab = self.weight_var * q_ab + self.bias_var * cov_init #ADD cov(X,X')
+                  q_ab = self.weight_var * q_ab + self.bias_var * cov_init_batch #ADD cov(X,X')
                   corr_flat_batch = q_ab / self.layer_qaa_dict[l + 1][0]
 
               q_ab_all.append(q_ab)
