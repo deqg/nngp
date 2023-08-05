b3c1592d41cb999c9efe68967f3362cbed067b74
deqg <132956809+deqg@users.noreply.github.com>
2023-08-05 11:54:46 -0500

    Update interp.py

--- interp.py
+++ interp.py
@@ -19,7 +19,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 
 def interp_lin(x, y, xp, log_spacing=False):
