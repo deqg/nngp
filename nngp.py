6299a2cf34355608618e1f280ea782cbdc10d453
deqg <132956809+deqg@users.noreply.github.com>
2023-08-05 15:22:41 -0500

    Update nngp.py

--- nngp.py
+++ nngp.py
@@ -219,13 +219,11 @@ class NNGPKernel(object):
           q_ab_all = []
           print(batch_count)
           for b_x in range(batch_count):
-            input1_batch = input1[batch_size * b_x : batch_size * (b_x + 1), :]
+            input1_batch = input1[batch_size * b_x : batch_size * (b_x + 1), :] ## Not sure whether the code works when batch_count >1
             input2_batch = input2[batch_size * b_x : batch_size * (b_x + 1), :]
-            print(corr.shape)
             with tf.name_scope("batch_%d" % b_x):
               corr_flat_batch = corr[
                   batch_size * b_x : batch_size * (b_x + 1), :]
-              print(corr_flat_batch.shape)
               corr_flat_batch = tf.reshape(corr_flat_batch, [-1])
               cov_init_batch = tf.matmul(input1_batch, input2_batch, transpose_b=True) / input1_batch.shape[1] #.value
               cov_init_batch = tf.reshape(cov_init_batch, [-1])
@@ -257,7 +255,8 @@ class NNGPKernel(object):
                                           z=self.qab_grid,
                                           xp=q_aa,
                                           yp=corr_flat)
-              q_ab = self.weight_var * q_ab + self.bias_var * cov_init # ADD cov(X.X')
+              cov_init_flat = tf.reshape(cov_init,[-1])
+              q_ab = self.weight_var * q_ab + self.bias_var * cov_init_flat # ADD cov(X.X')
               corr_flat = q_ab / self.layer_qaa_dict[l+1][0]
             q_ab_all = q_ab
 
