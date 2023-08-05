73e49f70c0406830c22bf66cd64fdbea6178e610
deqg <132956809+deqg@users.noreply.github.com>
2023-08-05 10:27:48 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,9 @@
+  class dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
 class AttrDict(dict):
     def __init__(self, *args, **kwargs):
         super(AttrDict, self).__init__(*args, **kwargs)
