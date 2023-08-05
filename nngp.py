86afd4ad11faaed2c10340aefc6657e9c195acbb
deqg <132956809+deqg@users.noreply.github.com>
2023-08-05 15:18:56 -0500

    Update nngp.py

--- nngp.py
+++ nngp.py
@@ -224,7 +224,7 @@ class NNGPKernel(object):
             with tf.name_scope("batch_%d" % b_x):
               corr_flat_batch = corr[
                   batch_size * b_x : batch_size * (b_x + 1), :]
-              print(coor_flat_batch.shape)
+              print(corr_flat_batch.shape)
               corr_flat_batch = tf.reshape(corr_flat_batch, [-1])
               cov_init_batch = tf.matmul(input1_batch, input2_batch, transpose_b=True) / input1_batch.shape[1] #.value
               cov_init_batch = tf.reshape(cov_init_batch, [-1])
