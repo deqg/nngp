2930a1005e5c97e95244d63002929f64a84e27a7
deqg <132956809+deqg@users.noreply.github.com>
2023-08-05 15:23:36 -0500

    Update nngp.py

--- nngp.py
+++ nngp.py
@@ -217,7 +217,6 @@ class NNGPKernel(object):
         batch_size, batch_count = self._get_batch_size_and_count(input1, input2)
         with tf.name_scope("q_ab"):
           q_ab_all = []
-          print(batch_count)
           for b_x in range(batch_count):
             input1_batch = input1[batch_size * b_x : batch_size * (b_x + 1), :] ## Not sure whether the code works when batch_count >1
             input2_batch = input2[batch_size * b_x : batch_size * (b_x + 1), :]
